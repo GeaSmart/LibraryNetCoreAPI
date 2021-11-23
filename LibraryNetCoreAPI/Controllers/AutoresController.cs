e5399b8f0f274c105e4cfeb7a52ed58cf426b102
Gerson Azabache <gerson_dunk@hotmail.com>
2021-11-23 14:23:19 -0500

    first controlller ready to update with best practices

---
+++ LibraryNetCoreAPI/Controllers/AutoresController.cs
@@ -0,0 +1,40 @@
+﻿using LibraryNetCoreAPI.Entidades;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryNetCoreAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AutoresController:ControllerBase
+    {
+        private readonly ApplicationDBContext context;
+
+        public AutoresController(ApplicationDBContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<List<Autor>> Get()
+        {
+            return await context.Autores.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] Autor autor)
+        {
+            var existe = await context.Autores.AnyAsync(x => x.Nombre == autor.Nombre);
+            if (existe)
+                return BadRequest($"Ya existe un autor con el nombre {autor.Nombre}");
+
+            context.Add(autor);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
