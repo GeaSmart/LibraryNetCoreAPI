c2af4ba2244c6154976644bc15412ad5fdf94507
Gerson Azabache Martinez <gerson_dunk@hotmail.com>
2021-12-17 01:44:10 -0500

    moq library feature added

--- LibraryNetCoreAPI.Tests/PruebasUnitarias/RootControllerTests.cs
+++ LibraryNetCoreAPI.Tests/PruebasUnitarias/RootControllerTests.cs
@@ -1,10 +1,13 @@
 ﻿using LibraryNetCoreAPI.Controllers.v1;
 using LibraryNetCoreAPI.Tests.Mocks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -32,7 +35,7 @@ namespace LibraryNetCoreAPI.Tests.PruebasUnitarias
         }
 
         [TestMethod]
-        public async Task SiUsuarioNoEsAdmin_Obtiene1Link()
+        public async Task SiUsuarioNoEsAdmin_Obtiene3Links()
         {
             //preparacion
             var authorizationService = new AuthorizationServiceMock();
@@ -44,6 +47,42 @@ namespace LibraryNetCoreAPI.Tests.PruebasUnitarias
             //ejecución de la prueba
             var resultado = await rootController.Get();
             
+            //verificación
+            Assert.AreEqual(3, resultado.Value.Count());
+
+        }
+
+        [TestMethod]
+        public async Task SiUsuarioNoEsAdmin_Obtiene3Links_usandoMoq()
+        {
+            //preparacion
+            //var authorizationService = new AuthorizationServiceMock();
+            //authorizationService.authorizationResult = AuthorizationResult.Failed();
+
+            var mockAuthorizationService = new Mock<IAuthorizationService>();
+            //para simular el método AuthorizeAsync
+            mockAuthorizationService.Setup(x => x.AuthorizeAsync(
+                It.IsAny<ClaimsPrincipal>(),
+                It.IsAny<object>(),
+                It.IsAny<IEnumerable<IAuthorizationRequirement>>()
+                )).Returns(Task.FromResult(AuthorizationResult.Failed()));
+
+            //para simular el método AuthorizeAsync sobrecargado
+            mockAuthorizationService.Setup(x => x.AuthorizeAsync(
+                It.IsAny<ClaimsPrincipal>(), 
+                It.IsAny<object>(),
+                It.IsAny<string>()
+                )).Returns(Task.FromResult(AuthorizationResult.Failed()));
+
+            var mockUrlHelper = new Mock<IUrlHelper>();
+            mockUrlHelper.Setup(x => x.Link(It.IsAny<string>(), It.IsAny<object>())).Returns(string.Empty);
+
+            var rootController = new RootController(mockAuthorizationService.Object);
+            rootController.Url = mockUrlHelper.Object;
+
+            //ejecución de la prueba
+            var resultado = await rootController.Get();
+
             //verificación
             Assert.AreEqual(3, resultado.Value.Count());
 
